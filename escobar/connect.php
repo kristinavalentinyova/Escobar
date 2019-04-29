e7f4463500b1270526ac86dedb480eb57ee5de31
Kristina Valentinyova <kristinavalentinyova@gmail.com>
2019-04-29 14:27:47 +0200

    connect odkomentovaný

--- escobar/connect.php
+++ escobar/connect.php
@@ -5,16 +5,16 @@ $username = "root";
 $password = "";
 $dbname = "test";
 
-//foreach ($_SERVER as $key => $value) {
-    //if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        //continue;
-    //}
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
     
-    //$servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    //$dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    //$username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    //$password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-//}
+    $servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
